e34c606673cb0b8808d993cdb11e30e53a0d65c4
Toan <nguyen.toan.0397@gmail.com>
2021-02-07 20:55:20 -0500

    Refactored code to reduce code in main

--- Program.cs
+++ Program.cs
@@ -1,66 +1,68 @@
 ﻿using System;
-using System.Collections; 
+using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
 namespace TechnicalAssignment
 {
     class Program
     {
+        //Email Regex
+        static readonly Regex PATTERN = new Regex(@"^[a-z]\w*(\.\w+)*@[\w]*[.][\w]*");
 
-        static void Main(string[] args)
+        static void PrintList(List<string> list)
+        {
+            list.ForEach(x => Console.WriteLine(x)); 
+        }
+
+        static void ValidateEmails()
         {
             //Asking user for file name
             Console.WriteLine("\nHello! Please enter the name of the .csv file:");
             string path = Console.ReadLine();
-            
-            ArrayList valid = new ArrayList();
-            ArrayList invalid = new ArrayList();
+
+            List<string> valid = new List<string>();
+            List<string> invalid = new List<string>();
 
             //Finding file
-            try 
+            try
             {
                 //Parsing file
-                var parser = new Microsoft.VisualBasic.FileIO.TextFieldParser(path); 
-                parser.SetDelimiters(new string[] {","});
+                var parser = new Microsoft.VisualBasic.FileIO.TextFieldParser(path);
+                parser.SetDelimiters(new string[] { "," });
                 parser.ReadLine();
 
-                Regex pattern = new Regex(@"^[a-z]\w*(\.\w+)*@[\w]*[.][\w]*");
-                while(!parser.EndOfData) 
+                while (!parser.EndOfData)
                 {
                     //Validating emails
                     string[] line = parser.ReadFields();
-                    if(pattern.IsMatch(line[2]))
+                    if (PATTERN.IsMatch(line[2]))
                     {
                         valid.Add(line[2]);
                     }
                     else
                     {
-                        invalid.Add(line[2]); 
+                        invalid.Add(line[2]);
                     }
                 }
 
                 //Printing valid and invalid emails
                 Console.WriteLine("\nValid Emails:");
-                foreach(string mail in valid)
-                {
-                    Console.WriteLine(mail);
-                }
+                PrintList(valid);
                 Console.WriteLine("\nInvalid Emails:");
-                foreach(string mail in invalid)
-                {
-                    Console.WriteLine(mail);
-                }
+                PrintList(invalid);
 
-            } 
-            catch(Exception) 
+            }
+            catch (Exception)
             {
                 Console.WriteLine("\nThe file name you entered was not found");
             }
-            finally 
-            {
-                Console.WriteLine("\nThank you for using this program\nPress any key to exit...");
-                Console.ReadKey(true);
-            }
+        }
+
+        static void Main(string[] args)
+        {
+            ValidateEmails(); 
+            Console.WriteLine("\nThank you for using this program\nPress any key to exit...");
+            Console.ReadKey(true);
         }
     }
 }
